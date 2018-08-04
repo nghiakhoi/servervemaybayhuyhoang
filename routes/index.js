276f4f3f270197bd7b52c10a36c42745bd482277
nghiakhoi <hnghiakhoi3@gmail.com>
2018-08-05 05:17:07 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -15,33 +15,28 @@ router.get('/js', function (req, res, next) {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  var options = {
-    method: 'POST',
-    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
-    qs: { airlines: 'js' },
-    headers:
-    {
-      'postman-token': '65091745-d6d7-e122-57bc-7eddcd2a422a',
-      'cache-control': 'no-cache',
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    form:
-    {
-      direction: '0',
-      loaive: '0',
-      depinput: 'Hồ Chí Minh (SGN)',
-      desinput: 'Đà Lạt (DLI)',
-      dep: 'SGN',
-      des: 'DLI',
-      depdate: '01-08-2018',
-      resdate: '02-08-2018',
-      adult: '1',
-      child: '0',
-      infant: '0',
-      cache: 'CXRHAN12-04-2018-04-20-37',
-      typeflight: '0'
-    }
-  };
+  var options = { method: 'POST',
+  url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+  qs: { airlines: 'js' },
+  headers: 
+   { 'postman-token': '24308256-514d-6510-fddf-9981f5296fae',
+     'cache-control': 'no-cache',
+     'content-type': 'application/x-www-form-urlencoded' },
+  form: 
+   { direction: '0',
+     loaive: '0',
+     depinput: 'Hồ Chí Minh (SGN)',
+     desinput: 'Hà Nội (HAN)',
+     dep: 'SGN',
+     des: 'HAN',
+     depdate: '01-09-2018',
+     resdate: '02-09-2018',
+     adult: '1',
+     child: '0',
+     infant: '0',
+     cache: 'CXRHAN12-04-2018-04-20-37',
+     typeflight: '0' } 
+    };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
@@ -63,33 +58,28 @@ router.get('/vj', function (req, res, next) {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  var options = {
-    method: 'POST',
-    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
-    qs: { airlines: 'vj' },
-    headers:
-    {
-      'postman-token': '65091745-d6d7-e122-57bc-7eddcd2a422a',
-      'cache-control': 'no-cache',
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    form:
-    {
-      direction: '0',
-      loaive: '0',
-      depinput: 'Hồ Chí Minh (SGN)',
-      desinput: 'Đà Lạt (DLI)',
-      dep: 'SGN',
-      des: 'DLI',
-      depdate: '01-08-2018',
-      resdate: '02-08-2018',
-      adult: '1',
-      child: '0',
-      infant: '0',
-      cache: 'CXRHAN12-04-2018-04-20-37',
-      typeflight: '0'
-    }
-  };
+  var options = { method: 'POST',
+  url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+  qs: { airlines: 'vj' },
+  headers: 
+   { 'postman-token': '24308256-514d-6510-fddf-9981f5296fae',
+     'cache-control': 'no-cache',
+     'content-type': 'application/x-www-form-urlencoded' },
+  form: 
+   { direction: '0',
+     loaive: '0',
+     depinput: 'Hồ Chí Minh (SGN)',
+     desinput: 'Hà Nội (HAN)',
+     dep: 'SGN',
+     des: 'HAN',
+     depdate: '01-09-2018',
+     resdate: '02-09-2018',
+     adult: '1',
+     child: '0',
+     infant: '0',
+     cache: 'CXRHAN12-04-2018-04-20-37',
+     typeflight: '0' } 
+    };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
@@ -111,33 +101,28 @@ router.get('/vn', function (req, res, next) {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  var options = {
-    method: 'POST',
-    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
-    qs: { airlines: 'vn' },
-    headers:
-    {
-      'postman-token': '65091745-d6d7-e122-57bc-7eddcd2a422a',
-      'cache-control': 'no-cache',
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    form:
-    {
-      direction: '0',
-      loaive: '0',
-      depinput: 'Hồ Chí Minh (SGN)',
-      desinput: 'Đà Lạt (DLI)',
-      dep: 'SGN',
-      des: 'DLI',
-      depdate: '01-08-2018',
-      resdate: '02-08-2018',
-      adult: '1',
-      child: '0',
-      infant: '0',
-      cache: 'CXRHAN12-04-2018-04-20-37',
-      typeflight: '0'
-    }
-  };
+  var options = { method: 'POST',
+  url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+  qs: { airlines: 'vn' },
+  headers: 
+   { 'postman-token': '24308256-514d-6510-fddf-9981f5296fae',
+     'cache-control': 'no-cache',
+     'content-type': 'application/x-www-form-urlencoded' },
+  form: 
+   { direction: '0',
+     loaive: '0',
+     depinput: 'Hồ Chí Minh (SGN)',
+     desinput: 'Hà Nội (HAN)',
+     dep: 'SGN',
+     des: 'HAN',
+     depdate: '01-09-2018',
+     resdate: '02-09-2018',
+     adult: '1',
+     child: '0',
+     infant: '0',
+     cache: 'CXRHAN12-04-2018-04-20-37',
+     typeflight: '0' } 
+    };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
