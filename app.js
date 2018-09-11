3be9ef9d5a17dbb590167e84df10fdecd47a4cc4
nghiakhoi <hnghiakhoi3@gmail.com>
2018-09-12 02:56:00 +0700

    update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  }else{
+  } else {
     corsOptions = { origin: false } // disable CORS for this request
   }
   callback(null, corsOptions) // callback expects two parameters: error and options
@@ -39,149 +39,172 @@ var corsOptionsDelegate = function (req, callback) {
 
 
 
-app.post('/vn',cors(corsOptionsDelegate), function (req, res, next) {
+app.post('/vn', cors(corsOptionsDelegate), function (req, res, next) {
 
-  
+
   var dep = req.body.dep;
   var des = req.body.des;
-  var datedep=req.body.datedep;
-  var depdate=req.body.depdate;
+  var datedep = req.body.datedep;
+  var depdate = req.body.depdate;
   //var datetime = req.body.datetime;
   var adult = req.body.adult;
-  
- console.log(req.body);
-  var options = { method: 'POST',
-  url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
-  qs: { airlines: 'vn' },
-  headers: 
-   { 'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
-     'cache-control': 'no-cache',
-     'content-type': 'application/x-www-form-urlencoded' },
-  form: 
-   { direction: '0',
-     loaive: '0',
-     depinput: '',
-     desinput: '',
-     dep: dep,
-     des: des,
-     depdate: datedep,
-     resdate: '',
-     adult: adult,
-     child: '0',
-     infant: '0',
-     cache: '',
-     typeflight: '0' } };
+  var child = req.body.child;
+  var inf = req.body.inf;
+
+  console.log(req.body);
+  var options = {
+    method: 'POST',
+    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+    qs: { airlines: 'vn' },
+    headers:
+    {
+      'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    form:
+    {
+      direction: '0',
+      loaive: '0',
+      depinput: '',
+      desinput: '',
+      dep: dep,
+      des: des,
+      depdate: datedep,
+      resdate: '',
+      adult: adult,
+      child: child,
+      infant: inf,
+      cache: '',
+      typeflight: '0'
+    }
+  };
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     //console.log(body + "ĐÂY LÀ JETSTAR");
     //res.send(body);
     //res.send(dep +" "+ des+" " + datetime+" " + adult);
-    console.log(dep +" "+ des+" " + datedep+" "+ +" " + adult );
+    console.log(dep + " " + des + " " + datedep + " " + +" " + adult);
     console.log(body);
     res.send(body);
   });
   //res.send(dep +" "+ des+" " + datetime+" " + adult);
-  
+
 });
 
-app.post('/js',cors(corsOptionsDelegate), function (req, res, next) {
+app.post('/js', cors(corsOptionsDelegate), function (req, res, next) {
 
-  
- var dep = req.body.dep;
- var des = req.body.des;
- var datedep = req.body.datedep;
- var datedes= req.body.datedes;
- var adult = req.body.adult;
- 
- console.log(req.body);
- var options = { method: 'POST',
- url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
- qs: { airlines: 'js' },
- headers: 
-  { 'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded' },
- form: 
-  { direction: '0',
-    loaive: '0',
-    depinput: '',
-    desinput: '',
-    dep: dep,
-    des: des,
-    depdate: datedep,
-    resdate: '',
-    adult: adult,
-    child: '0',
-    infant: '0',
-    cache: '',
-    typeflight: '0' } };
 
- request(options, function (error, response, body) {
-   if (error) throw new Error(error);
-   //console.log(body + "ĐÂY LÀ JETSTAR");
-   //res.send(body);
-   //res.send(dep +" "+ des+" " + datetime+" " + adult);
-   console.log(dep +" "+ des+" " + datedep+" "+ +" " + adult);
-   console.log(body);
-   res.send(body);
- });
- //res.send(dep +" "+ des+" " + datetime+" " + adult);
- 
+  var dep = req.body.dep;
+  var des = req.body.des;
+  var datedep = req.body.datedep;
+  var datedes = req.body.datedes;
+  var adult = req.body.adult;
+  var child = req.body.child;
+  var inf = req.body.inf;
+
+  console.log(req.body);
+  var options = {
+    method: 'POST',
+    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+    qs: { airlines: 'js' },
+    headers:
+    {
+      'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    form:
+    {
+      direction: '0',
+      loaive: '0',
+      depinput: '',
+      desinput: '',
+      dep: dep,
+      des: des,
+      depdate: datedep,
+      resdate: '',
+      adult: adult,
+      child: child,
+      infant: inf,
+      cache: '',
+      typeflight: '0'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    //console.log(body + "ĐÂY LÀ JETSTAR");
+    //res.send(body);
+    //res.send(dep +" "+ des+" " + datetime+" " + adult);
+    console.log(dep + " " + des + " " + datedep + " " + +" " + adult);
+    console.log(body);
+    res.send(body);
+  });
+  //res.send(dep +" "+ des+" " + datetime+" " + adult);
+
 });
 
-app.post('/vj',cors(corsOptionsDelegate), function (req, res, next) {
+app.post('/vj', cors(corsOptionsDelegate), function (req, res, next) {
 
-  
- var dep = req.body.dep;
- var des = req.body.des;
- var datedep = req.body.datedep;
- var datedes = req.body.datedes;
- console.log(req.body);
- var adult = req.body.adult;
- var options = { method: 'POST',
- url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
- qs: { airlines: 'vj' },
- headers: 
-  { 'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded' },
- form: 
-  { direction: '0',
-    loaive: '0',
-    depinput: '',
-    desinput: '',
-    dep: dep,
-    des: des,
-    depdate: datedep,
-    resdate: '',
-    adult: adult,
-    child: '0',
-    infant: '0',
-    cache: '',
-    typeflight: '0' } };
 
- request(options, function (error, response, body) {
-   if (error) throw new Error(error);
-   //console.log(body + "ĐÂY LÀ JETSTAR");
-   //res.send(body);
-   //res.send(dep +" "+ des+" " + datetime+" " + adult);
-   console.log(dep +" "+ des+" " + datedep+" "+ +" " + adult);
-   console.log(body);
-   res.send(body);
- });
- //res.send(dep +" "+ des+" " + datetime+" " + adult);
- 
+  var dep = req.body.dep;
+  var des = req.body.des;
+  var datedep = req.body.datedep;
+  var datedes = req.body.datedes;
+  var adult = req.body.adult;
+  var child = req.body.child;
+  var inf = req.body.inf;
+  var options = {
+    method: 'POST',
+    url: 'http://vebaygiare247.vn/vebaygiare247.vn/tim-ve',
+    qs: { airlines: 'vj' },
+    headers:
+    {
+      'postman-token': 'e991929b-b677-ecb1-e766-d55255b0af77',
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    form:
+    {
+      direction: '0',
+      loaive: '0',
+      depinput: '',
+      desinput: '',
+      dep: dep,
+      des: des,
+      depdate: datedep,
+      resdate: '',
+      adult: adult,
+      child: child,
+      infant: inf,
+      cache: '',
+      typeflight: '0'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    //console.log(body + "ĐÂY LÀ JETSTAR");
+    //res.send(body);
+    //res.send(dep +" "+ des+" " + datetime+" " + adult);
+    console.log(dep + " " + des + " " + datedep + " " + +" " + adult);
+    console.log(body);
+    res.send(body);
+  });
+  //res.send(dep +" "+ des+" " + datetime+" " + adult);
+
 });
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
