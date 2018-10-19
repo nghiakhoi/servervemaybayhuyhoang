0abbed09c90621515380cc56c804b3ccebcefc68
nghiakhoi <hnghiakhoi3@gmail.com>
2018-10-20 04:33:15 +0700

    update

--- app.js
+++ app.js
@@ -49,7 +49,13 @@ var corsOptionsDelegate = function (req, callback) {
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
-
+function makeidrandom() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (var i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
 
 app.post('/vn', cors(corsOptionsDelegate), function (req, res, next) {
 
@@ -196,20 +202,117 @@ app.post('/infobooking', cors(corsOptionsDelegate), function (req, res, next) {
   var address = req.body.address;
   var email = req.body.email;
   var yeucau = req.body.yeucau;
-  var hinhthucthanhtoan = req.body.hinhthucthanhtoan;
+  var hinhthucthanhtoan = req.body.hinhthucthanhtoan === "chuyenkhoan" ? req.body.nganhangchoosed : req.body.hinhthucthanhtoan;
   var subtotaloriginal = req.body.subtotaloriginal;
   var subtotalwithhanhly = req.body.subtotalwithhanhly;
+  var thongtinvedi = req.body.thongtinvedi ? JSON.stringify(req.body.thongtinvedi) : "";
+  var thongtinveKhuHoi = req.body.thongtinveKhuHoi;
+  var mangadult = req.body.mangadult;
+  var mangchild = req.body.mangchild;
+  var manginf = req.body.manginf;
   var create_time = moment().format("DD-MM-YYYY HH:mm:ss");
+  var ketquadiInsert = false;
+  var ketquaKhuHoiInsert = false;
+  var randomcode = makeidrandom();
+  console.log(mangadult);
   console.log(thongtinvedi);
   console.log(thongtinveKhuHoi);
-  pool.query("INSERT INTO donhang (fullname,phone,address,email,yeucau,hinhthucthanhtoan,subtotalorigin,subtotalwithhanhly,create_date) VALUES ('" + fullname + "', '" + phone + "','" + address + "','" + email + "','" + yeucau + "','" + hinhthucthanhtoan + "','" + subtotaloriginal + "','" + subtotalwithhanhly + "','" + create_time + "') RETURNING id")
+  pool.query("INSERT INTO donhang (fullname,phone,address,email,yeucau,hinhthucthanhtoan,subtotalorigin,subtotalwithhanhly,create_date,code) VALUES ('" + fullname + "', '" + phone + "','" + address + "','" + email + "','" + yeucau + "','" + hinhthucthanhtoan + "','" + subtotaloriginal + "','" + subtotalwithhanhly + "','" + create_time + "','" + randomcode + "') RETURNING id")
     .then(result => {
-      console.log(result.rows[0]);
-      res.send("ok");
+      if (thongtinvedi !== null) {
+        var param1 = [thongtinvedi, 'di', parseInt(result.rows[0].id)];
+        pool.query('INSERT INTO chuyenbay (jsonchuyenbay,loaichuyenbay,iddonhang) VALUES ($1,$2,$3) RETURNING id', param1)
+          .then(result => {
+            if (mangadult.length !== 0) {
+              for (var i = 0; i < mangadult.length; i++) {
+                var param2 = ['adult', mangadult[i].quydanhadult, mangadult[i].hoadult, mangadult[i].demvatenadult, "", mangadult[i].hanhlyadult, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquadiInsert = true;
+                  })
+              }
+            }
+            if (mangchild.length !== 0) {
+              for (var i = 0; i < mangchild.length; i++) {
+                var param2 = ['child', mangchild[i].quydanhchild, mangchild[i].hochild, mangchild[i].demvatenchild, mangchild[i].ngaysinhchild, mangchild[i].hanhlychild, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquadiInsert = true;
+                  })
+              }
+            }
+            if (manginf.length !== 0) {
+              for (var i = 0; i < manginf.length; i++) {
+                var param2 = ['inf', manginf[i].quydanhinf, manginf[i].hoinf, manginf[i].demvateninf, manginf[i].ngaysinhinf, 1, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquadiInsert = true;
+                  })
+              }
+            }
+          })
+      }
+      if (thongtinveKhuHoi !== null) {
+        var param1 = [thongtinveKhuHoi, 'khuhoi', parseInt(result.rows[0].id)];
+        pool.query('INSERT INTO chuyenbay (jsonchuyenbay,loaichuyenbay,iddonhang) VALUES ($1,$2,$3) RETURNING id', param1)
+          .then(result => {
+            if (mangadult.length !== 0) {
+              for (var i = 0; i < mangadult.length; i++) {
+                var param2 = ['adult', mangadult[i].quydanhadult, mangadult[i].hoadult, mangadult[i].demvatenadult, "", mangadult[i].hanhlyadult, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquaKhuHoiInsert = true;
+                  })
+              }
+            }
+            if (mangchild.length !== 0) {
+              for (var i = 0; i < mangchild.length; i++) {
+                var param2 = ['child', mangchild[i].quydanhchild, mangchild[i].hochild, mangchild[i].demvatenchild, mangchild[i].ngaysinhchild, mangchild[i].hanhlychild, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquaKhuHoiInsert = true;
+                  })
+              }
+            }
+            if (manginf.length !== 0) {
+              for (var i = 0; i < manginf.length; i++) {
+                var param2 = ['inf', manginf[i].quydanhinf, manginf[i].hoinf, manginf[i].demvateninf, manginf[i].ngaysinhinf, 1, parseInt(result.rows[0].id)];
+                pool.query('INSERT INTO hanhkhach (loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh,idhanhly,idchuyenbay) VALUES ($1,$2,$3,$4,$5,$6,$7)', param2)
+                  .then(result => {
+                    ketquaKhuHoiInsert = true;
+                  })
+              }
+            }
+          })
+      }
+      return result;
+    }).then(result => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({ result: "ok", id: result.rows[0].id }, null, 3));
     })
     .catch(e => console.error(e.stack));
+});
 
-  
+app.post('/getinvoice', cors(corsOptionsDelegate), function (req, res, next) {
+  var idhoadon = req.body.idhoadon;
+  pool.query("SELECT fullname,phone,address,email,yeucau,hinhthucthanhtoan,subtotalorigin,subtotalwithhanhly,donhang.code,jsonchuyenbay,loaichuyenbay,loaihanhkhach,quydanh,ho,tendemvaten,ngaysinh FROM  donhang INNER JOIN chuyenbay ON chuyenbay.iddonhang = donhang.id INNER JOIN hanhkhach ON hanhkhach.idchuyenbay = chuyenbay.id where donhang.id=$1 order by loaihanhkhach", [idhoadon])
+    .then(result => {
+      if (result.rows.length === 0) {
+        return false;
+      } else {
+        return result;
+      }
+    }).then(result => {
+      if (result === false) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ result: "fail" }, null, 3));
+      } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ result: "ok", data: result.rows }, null, 3));
+      }
+
+    })
+    .catch(e => console.error(e.stack));
 });
 
 
